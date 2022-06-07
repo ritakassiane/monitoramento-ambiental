83e459a3e09343a39585e5739b9b1fe4a0ff66ec
Rita Kassiane <ritakassiane.t.i@gmail.com>
2022-06-06 23:38:51 -0300

    Criação do componente Main e Category

---
+++ interface-remota/vue/pbl03/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Main/>
+
+  </div>
+</template>
+
+<script>
+import './styles/global.css';
+import Main from './components/Main';
+
+
+
+export default {
+  name: 'App',
+  components: {
+    Main
+  }
+}
+</script>
